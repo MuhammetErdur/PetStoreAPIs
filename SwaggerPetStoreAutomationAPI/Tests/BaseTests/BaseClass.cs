96e813eb69b016af0ed3f8c529b1153ceed9ab33
MuhammetErdur <100716159+MuhammetErdur@users.noreply.github.com>
2023-08-02 17:43:56 +0300

    Add files via upload

---
+++ SwaggerPetStoreAutomationAPI/Tests/BaseTests/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SwaggerPetStoreAutomationAPI;
+using SwaggerPetStoreAutomationAPI.Actions;
+
+namespace SwaggerPetStoreAutomationTests.BaseTests
+{
+    public abstract class BaseClass : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+        protected IDisposable logCapture;
+        internal readonly ActionFactory Actions = new ActionFactory();
+        internal readonly SharedStepFactory SharedSteps = new SharedStepFactory();
+
+        protected BaseClass(ITestOutputHelper outputHelper)
+        {
+            _output = outputHelper;
+            logCapture = new LoggingHelper().Capture(_output);
+        }
+
+        public void Dispose()
+        {
+            logCapture.Dispose();
+        }
+    }
+}
